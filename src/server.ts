532ccc497168a66c768a1ff940506f73beda149d
Paulo Ricardo <1981928paulo@gmail.com>
2022-10-20 11:48:51 -0300

    configurando prettier

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,30 @@
 import './util/module-alias';
 
-import {Server} from '@overnightjs/core';
+import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
-export class SetupServer extends Server{
-    constructor(private port = 3000){
-        super();
-    }
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
 
-    public init(): void{
-        this.setupExpress();
-        this.setUpControllers()
-    }
-    
-    private setupExpress():void{
-        this.app.use(bodyParser.json());
-        
-    }   
+  public init(): void {
+    this.setupExpress();
+    this.setUpControllers();
+  }
 
-    private setUpControllers(): void{
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-    public getApp(): Application{
-        return this.app;
-    }
+  private setUpControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
 }
--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -2,9 +2,9 @@ import * as path from 'path';
 import moduleAlias from 'module-alias';
 
 // permite que os alias @src e @test sejam alcançados por todo o projeto
-const files = path.resolve(__dirname,'../..');
+const files = path.resolve(__dirname, '../..');
 
 moduleAlias.addAliases({
-    '@src':path.join(files, 'src'),
-    '@test': path.join(files,'test')
-})
+  '@src': path.join(files, 'src'),
+  '@test': path.join(files, 'test'),
+});
